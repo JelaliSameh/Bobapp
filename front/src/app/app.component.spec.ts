82b87a8941fc81b06d557639629aca125d5d8088
JelaliSameh <jelali.sameh@gmail.com>
2025-05-14 13:04:33 +0200

    Mise à jour du code avec les dernières fonctionnalités

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,12 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {JokesService} from "./services/jokes.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, MatToolbarModule,
+      MatCardModule],
       providers: [JokesService]
     }).compileComponents();
   });
